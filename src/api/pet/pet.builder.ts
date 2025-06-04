bee1fe52a5c9b7c29a9e1b6442570e45c887f4c8
Ivan Druzhchenko <ivan.druzhchenko@sysgears.com>
2025-06-04 11:18:54 +0300

    feat: added builder class for pet instance

---
+++ src/api/pet/pet.builder.ts
@@ -0,0 +1,53 @@
+import { PetInterface, PetStatus } from './pet.interfaces'
+
+export class PetBuilder {
+  private pet: PetInterface
+
+  constructor() {
+    this.pet = {
+      id: 0,
+      name: '',
+      status: 'available', // Default status
+    }
+  }
+
+  setId(id: number): PetBuilder {
+    this.pet.id = id
+    return this
+  }
+
+  setCategory(id: number, name: string): PetBuilder {
+    this.pet.category = { id, name }
+    return this
+  }
+
+  setName(name: string): PetBuilder {
+    this.pet.name = name
+    return this
+  }
+
+  setPhotoUrls(photoUrls: string[]): PetBuilder {
+    this.pet.photoUrls = photoUrls
+    return this
+  }
+
+  setTags(tags: { id: number; name: string }[]): PetBuilder {
+    this.pet.tags = tags
+    return this
+  }
+
+  setStatus(status: PetStatus): PetBuilder {
+    this.pet.status = status
+    return this
+  }
+
+  build(): PetInterface {
+    if (!this.pet.name) {
+      throw new Error('Pet name is required')
+    }
+    if (!this.pet.status) {
+      throw new Error('Status is required')
+    }
+    return this.pet
+  }
+}
