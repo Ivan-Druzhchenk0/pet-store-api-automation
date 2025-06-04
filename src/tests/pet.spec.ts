869f6d283d348744544fa17ac2e82dbb257bd940
Ivan Druzhchenko <ivan.druzhchenko@sysgears.com>
2025-06-04 21:09:32 +0300

    tests: updated existing tests

--- src/tests/pet.spec.ts
+++ src/tests/pet.spec.ts
@@ -3,22 +3,21 @@ import { test } from '../fixtures'
 
 import { PetStatus } from '../api/pet/pet.interfaces'
 import { PetBuilder } from '../api/pet/pet.builder'
+import { petId, petName } from '../test-data/pet'
 
 test.describe('CRUD pet by id', () => {
   test.describe.configure({ mode: 'serial' })
 
   // Timeout needed as API is slow and tests fail without it
   test.afterEach(async () => {
-    // Wait 2 seconds before each test
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) => setTimeout(resolve, 8000))
   })
 
-  const petId = 11
-  const petName = 'Buddy'
+  const id = petId().id
+  const name = petName().name
+  const newPet = new PetBuilder().setId(id).setName(name).build()
 
   test('Create a pet with id', async ({ api }) => {
-    const newPet = new PetBuilder().setId(petId).setName(petName).build()
-
     const response = await api.pet.addNewPet(newPet)
 
     const responseBody = await response.json()
@@ -28,38 +27,38 @@ test.describe('CRUD pet by id', () => {
   })
 
   test('Get a pet by ID', async ({ api }) => {
-    const response = await api.pet.getPetById(petId)
+    const response = await api.pet.getPetById(id)
 
     const responseBody = await response.json()
     expect(response.status()).toBe(200)
-    expect(responseBody.id).toBe(petId)
-    expect(responseBody.name).toBe(petName)
+    expect(responseBody.id).toBe(newPet.id)
+    expect(responseBody.name).toBe(newPet.name)
   })
 
   test('Update a pet', async ({ api }) => {
     const updatedPet = { status: 'available' as PetStatus }
 
     const response = await api.pet.updatePet({
-      petId: petId,
+      petId: id,
       petData: updatedPet,
     })
 
     const responseBody = await response.json()
     expect(response.status()).toBe(200)
-    expect(responseBody.id).toBe(petId)
-    expect(responseBody.name).toBe(petName)
+    expect(responseBody.id).toBe(newPet.id)
+    expect(responseBody.name).toBe(newPet.name)
     expect(responseBody.status).toBe(updatedPet.status)
   })
 
-  test('Detete a pet', async ({ api }) => {
-    const response = await api.pet.deletePet(petId)
+  test('Delete a pet', async ({ api }) => {
+    const response = await api.pet.deletePet(id)
 
     expect(response.status()).toBe(200)
   })
 })
 
 test('Create a pet without id', async ({ api }) => {
-  const newPetWithoutId = new PetBuilder().setName('Charlie').build()
+  const newPetWithoutId = new PetBuilder().setName(petName().name).build()
 
   const response = await api.pet.addNewPet(newPetWithoutId)
 
