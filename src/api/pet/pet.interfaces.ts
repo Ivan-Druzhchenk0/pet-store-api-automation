243dbe2acb80bdf818e44b005b25bb4c0e840286
Ivan Druzhchenko <ivan.druzhchenko@sysgears.com>
2025-06-04 11:18:25 +0300

    feat: added interface for pet instance

---
+++ src/api/pet/pet.interfaces.ts
@@ -0,0 +1,16 @@
+export type PetStatus = 'available' | 'pending' | 'sold'
+
+export interface PetInterface {
+  id: number
+  category?: {
+    id: number
+    name: string
+  }
+  name: string
+  photoUrls?: string[]
+  tags?: {
+    id: number
+    name: string
+  }[]
+  status: PetStatus
+}
