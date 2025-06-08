7dcf44857005c62e61999d057f286dfb67a8cc4e
Ivan Druzhchenko <ivan.druzhchenko@sysgears.com>
2025-06-08 10:18:17 +0300

    fix: updated pet factory method

--- src/test-data/factory/pet.factory.ts
+++ src/test-data/factory/pet.factory.ts
@@ -1,4 +1,5 @@
 import { PetBuilder } from '../../api/pet/pet.builder'
+import { PetStatus } from '../../api/pet/pet.interfaces'
 import { petId, PetInfo, petName } from '../entities/pet'
 
 export class PetFactory {
@@ -14,9 +15,16 @@ export class PetFactory {
       .build()
   }
 
-  static createPetWithoutId(name?: string) {
+  static createPetWithoutId(
+    name?: string,
+    status?: 'available' | 'pending' | 'sold',
+  ) {
     const uniqueName = petName().name || name
+    const uniqueStatus = status || 'available'
 
-    return new PetBuilder().setName(uniqueName!).setStatus('available').build()
+    return new PetBuilder()
+      .setName(uniqueName!)
+      .setStatus(uniqueStatus as PetStatus)
+      .build()
   }
 }
